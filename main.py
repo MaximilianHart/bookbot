277a7a6c065a382b864a8913efd28d757800eaa1
Maximilian Hart <38765373+MaximilianHart@users.noreply.github.com>
2025-08-01 17:28:30 -0500

    feat: sorted & iterating over characters

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from stats import get_num_words
+from stats import get_num_words, get_letters, sort_dict
 
 
 def main():
     get_num_words("./books/frankenstein.txt")
+    get_letters("./books/frankenstein.txt")
+    sort_dict()
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,37 @@
+letters_dict = {}
+sorted_dicts = []
+new_dict = {}
+
+
 def get_num_words(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         num_words = len(file_contents.split())
         print(f"{num_words} words found in the document")
+
+
+def get_letters(file_path):
+    with open(file_path) as f:
+        file_contents = str.lower(f.read())
+    for key in file_contents:
+        if key not in letters_dict:
+            letters_dict[key] = 1
+        else:
+            letters_dict[key] += 1
+    return letters_dict
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def sort_dict():
+    for char in letters_dict:
+        if char.isalpha():
+            num = letters_dict[char]
+            sorted_dicts.append({"char": char, "num": num})
+    sorted_dicts.sort(reverse=True, key=sort_on)
+    for dict in sorted_dicts:
+        char = dict["char"]
+        num = dict["num"]
+        print(f"{char}: {num}")
